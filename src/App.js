25f9c167e50795a1b2e31abcf5c623469e24c821
Daniel <ventura.daniel36@gmail.com>
2023-12-06 21:11:17 -0700

    login working

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
 import './App.css';
-import { useState  } from "react"
+import { useState, useEffect  } from "react"
 import {EventCalendar} from './components/EventCalendar'
 import {Calendars} from './components/Calendars'
 import Login from './components/Auth/Login'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
 
 function App() {
   const [token, setToken] = useState();
   const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")) ?? null)
 
-  if(!user) {
-    return <Login userDataApp={user} />
-  } 
-  if(user){
-      return <div>{user.username} is loggged in</div>;
-  }
+  const navigate = useNavigate();
+
+
+  const navigateToCalendar = () => {
+    navigate('/calendar');
+  };
+
+  const navigateToLogin = () => {
+    navigate('/');
+  };
+
+  useEffect(() => {
+    if(!user) {
+      navigateToLogin()    
+    } 
+    if(user){
+      navigateToCalendar()
+    }
+  }, [])
+
+
   
   return (
     <div className="App">
           <div className="wrapper">
             <h1>Application</h1>
               <Routes>
-                <Route path="/" element={<Login />} />
+                <Route path="*" element={<Login />} />
                 <Route path="/calendar" element={<Calendars />} />
                 <Route path="/calendar/:calendarID" element={<EventCalendar />} />
               </Routes>
